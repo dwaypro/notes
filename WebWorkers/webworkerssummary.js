3b9428f5f34543123edbc21ec8aa4f284a43fe0a
dwaypro <dangrantway@gmail.com>
2018-07-23 23:51:17 -0700

    asynchronous callback mechanisms

--- WebWorkers/webworkerssummary.js
+++ WebWorkers/webworkerssummary.js
@@ -1,3 +1,7 @@
 //Javascript is always synchronous and single-threaded
 
-//
+//There is a lot of confusion circling around this idea
+
+//because JavaScript is synchronous there are various callback mechanisms in place
+//that help it perform asynchronous operations.
+
